793346813de1231071e4a7d47a85c8f666eaa074
Sunny Singh <sunnysinghnitb@gmail.com>
2021-01-21 01:53:31 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60089022d34e73001fe65765",
+				"60089022d34e73001fe65767"
+			]
+		},
+		"60089022d34e73001fe65765": {
+			"id": "60089022d34e73001fe65765",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60089022d34e73001fe65767": {
+			"id": "60089022d34e73001fe65767",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
